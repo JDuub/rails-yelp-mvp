62a7c40800f806f53e4d10d765d5f42dbf334c11
Jannis Weber <jannis.weber@gmx.de>
2023-02-22 17:26:22 +0100

    rails_yelp_mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   get "/restaurants", to: "restaurants#index"
+#   get "/restaurants/new", to: "restaurants#new"
+#   post "/restaurants", to: "restaurants#create"
+#   get "/restaurants/:id", to: "restaurants#show", as: :restaurant
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+
+Rails.application.routes.draw do
+  get 'reviews/new'
+  resources :restaurants do
+    collection do
+      get :top
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews
 end
